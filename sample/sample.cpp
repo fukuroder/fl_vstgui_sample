a4e0a32d9efa88bfc57951344dcca621710c45db
fukuroda <fukuroder@live.jp>
2013-03-17 10:55:12 +0900

    SetParentするように戻した

--- sample/sample.cpp
+++ sample/sample.cpp
@@ -100,17 +100,22 @@ int _stdcall sample::Dispatcher(intptr_t ID, intptr_t Index, intptr_t Value)
 			_editor = new sample_editor(this);
 		}
 
-		if (Value == 0)
-		{
+        if (Value == 0)
+        {
 			// close editor
 			_editor->close();
 			EditorHandle = 0;
-		}
-		else
+        }
+        else if( EditorHandle == 0 )
 		{
 			// open editor
 			_editor->open(reinterpret_cast<HWND>(Value));
 			EditorHandle = static_cast<HWND>(_editor->getFrame()->getPlatformFrame()->getPlatformRepresentation());
+		}
+		else
+		{
+			// change parent window ?
+			::SetParent(EditorHandle, reinterpret_cast<HWND>(Value));
 		}
 	}
 	return 0;
